7a77cb96e2eb7c3a971fe20a735a02f8834aaf57
Daniel Trostli <daniel.trostli@gmail.com>
2014-10-14 23:14:26 -0700

    Cleaned up code

--- create_user_csv.rb
+++ create_user_csv.rb
@@ -1,8 +1,7 @@
 require 'csv'
-require 'securerandom'
 
 CSV.open("users.csv", "wb") do |csv|
-  1000.times do
+  2000.times do
     csv << [rand(500000000000),rand(500000000000)]
   end
 end
--- cs_challenge_neo4j.rb
+++ cs_challenge_neo4j.rb
@@ -5,7 +5,6 @@
 require 'neo4j'
 require 'csv'
 
-array_of_user_id_tuples = CSV.read("users.csv")
 @invalid_relationships = []
 
 Neo4j::Session.open(:server_db)
@@ -15,6 +14,21 @@ class User
   property :user_id, type: Integer, constraint: :unique, index: :exact
 
   has_many :both, :friends, model_class: User
+
+  def self.create_or_find_user(user_id)
+    user = User.find_by(user_id: user_id)
+    if user == nil
+      user = User.create(user_id: user_id)
+    end
+    return user
+  end
+
+end
+
+def create_relationship(user_id_tuple)
+  user1 = User.create_or_find_user(user_id_tuple.first.to_i)
+  user2 = User.create_or_find_user(user_id_tuple.last.to_i)
+  user1.friends << user2
 end
 
 def invalid?(user_id_tuple)
@@ -26,23 +40,9 @@ def invalid?(user_id_tuple)
   end
 end
 
-def create_or_find_user(user_id)
-  user = User.find_by(user_id: user_id)
-  if user == nil
-    user = User.create(user_id: user_id)
-  end
-  return user
-end
-
-def create_relationship(user_id_tuple)
-  user1 = create_or_find_user(user_id_tuple.first.to_i)
-  user2 = create_or_find_user(user_id_tuple.last.to_i)
-  user1.friends << user2
-end
-
-array_of_user_id_tuples.each_with_index do |user_id_tuple, index|
+CSV.foreach("users.csv") do |user_id_tuple|
   if invalid?(user_id_tuple)
-    puts "Invalid relationship on line #{index+1} found!"
+    puts "Invalid relationship: #{user_id_tuple}"
   else
     create_relationship(user_id_tuple)
   end
